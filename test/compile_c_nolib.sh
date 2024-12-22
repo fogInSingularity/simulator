3ed2f36e432b2c739dd7d8548371fc647eb580e5
fogInSingularity <vladimirnaumov979@gmail.com>
2024-12-22 17:48:37 +0300

    fix script

--- test/compile_c_nolib.sh
+++ test/compile_c_nolib.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-~/code/sims/ricsv-toolchain/toolchain/bin/riscv64-unknown-elf-gcc -nostartfiles -Og -nostdlib $1 -o $1.asm -mabi=ilp32 -march=rv32i -S
-~/code/sims/ricsv-toolchain/toolchain/bin/riscv64-unknown-elf-as -mabi=ilp32 -march=rv32i $1.asm -o $1.o
-~/code/sims/ricsv-toolchain/toolchain/bin/riscv64-unknown-elf-ld $1.o -o $1.out -melf32lriscv
+./../../ricsv-toolchain/toolchain/bin/riscv64-unknown-elf-gcc -nostartfiles -Og -nostdlib $1 -o $1.asm -mabi=ilp32 -march=rv32i -S
+./../../ricsv-toolchain/toolchain/bin/riscv64-unknown-elf-as -mabi=ilp32 -march=rv32i $1.asm -o $1.o
+./../../ricsv-toolchain/toolchain/bin/riscv64-unknown-elf-ld $1.o -o $1.out -melf32lriscv
 
 rm $1.asm $1.o
 
